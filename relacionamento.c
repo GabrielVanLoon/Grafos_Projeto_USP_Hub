c6a3398301331d4029040a0badb7ec3bd8320a03
AlbertWolf99 <alberto.cneves@gmail.com>
2019-06-04 18:25:27 -0300

    Feita parte do relacionamento

--- relacionamento.c
+++ relacionamento.c
@@ -42,3 +42,38 @@ int rel_lerRelacionamentos(Relacionamento* rel, int id){
 	fclose(fp);
 	return 0;
 }
+
+/**
+*
+*@return 1: Erro de abertura de arquivo
+*@return 2: Numero máximo de amigos excedido
+**/
+int rel_addAmizade (Relacionamento* rel, int id, int idAmigo, int pontosAmigo){
+	if(rel_lerRelacionamentos(rel, id)) return 1;
+	if(rel->nroRelacionamento >= 99) return 2;
+	rel->amizades[rel->nroRelacionamento].id = idAmigo;
+	rel->amizades[rel->nroRelacionamento].pontos = pontosAmigo;
+	rel->nroRelacionamento++;
+	//TODO: heapsort pelos pontos
+	if(rel_escreverRelacionamento(rel, id)) return 1;
+	return 0;
+}
+
+/**
+*
+*@return 1: Erro de abertura de arquivo
+*@return 2: Nao ha amigos
+**/
+int rel_removeAmizade (Relacionamento* rel, int id, int idAmigo){
+	if(rel_lerRelacionamentos(rel, id)) return 1;
+	if(rel->nroRelacionamento <= 0) return 2;
+	int pos;
+	//TODO: heapsort pelo id
+	//TODO: if(pos = busca bunaria(id), pos != -1) //remove
+	rel->amizades[pos].id = 0;
+	rel->amizades[pos].pontos = 0;
+	//TODO: heapsort pelos pontos
+	rel->nroRelacionamento--;
+	if(rel_escreverRelacionamento(rel, id)) return 1;
+	return 0;
+}
